034fb90ed8fcb351d417b6e61d2d3197e47c26f8
Igor Balandin <igor.balandin@gmx.de>
2025-02-03 09:33:29 +0100

    mysql and mail properties.

---
+++ src/main/java/com/bal/demo_reddit/DemoRedditApplication.java
@@ -0,0 +1,15 @@
+package com.bal.demo_reddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class DemoRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRedditApplication.class, args);
+	}
+
+}
