6838ed16f599f730c8929576ca9d19a3f09a748d
Igor Balandin <igor.balandin@gmx.de>
2025-02-03 09:11:04 +0100

    Controllers for Signup and account Verification.

---
+++ src/main/java/com/bal/demo_reddit/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.bal.demo_reddit.controller;
+
+import com.bal.demo_reddit.dto.RegisterRequest;
+import com.bal.demo_reddit.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration successful!", HttpStatus.OK);
+    }
+
+    @GetMapping("accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("Account Activated successfully", HttpStatus.OK);
+    }
+}
